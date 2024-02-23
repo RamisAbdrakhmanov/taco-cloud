46f6676b54d289edd4bec998397282627c242d67
Ramis <115800392+RamisAbdrakhmanov@users.noreply.github.com>
2024-02-23 14:33:16 +0300

    add JPA

--- src/main/java/com/project/tacocloud/model/Ingredient.java
+++ src/main/java/com/project/tacocloud/model/Ingredient.java
@@ -1,20 +1,23 @@
 package com.project.tacocloud.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import lombok.NoArgsConstructor;
+
 
 @Data
-@Table
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access= AccessLevel.PUBLIC, force=true)
 public class Ingredient {
     @Id
-    private final String id;
-    private final String name;
-    private final Type type;
-
+    private String id;
+    private String name;
+    private Type type;
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
-
-
 }
--- src/main/java/com/project/tacocloud/repository/OrderRepository.java
+++ src/main/java/com/project/tacocloud/repository/OrderRepository.java
@@ -3,5 +3,11 @@ package com.project.tacocloud.repository;
 import com.project.tacocloud.model.TacoOrder;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface OrderRepository extends CrudRepository<TacoOrder, Long> {
+    List<TacoOrder> findAllByDeliveryZip(String deliveryZip);
+    List<TacoOrder> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+    List<TacoOrder> findByDeliveryCityOrderById(String city);
 }
