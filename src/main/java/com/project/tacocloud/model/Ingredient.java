f6a7a59aa0fafe620bc4d197ddaaf7833c696e01
Ramis <115800392+RamisAbdrakhmanov@users.noreply.github.com>
2024-02-23 14:04:30 +0300

    add JdbcTemplate

--- src/main/java/com/project/tacocloud/model/Ingredient.java
+++ src/main/java/com/project/tacocloud/model/Ingredient.java
@@ -1,14 +1,17 @@
 package com.project.tacocloud.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 @Data
-@AllArgsConstructor
+@Table
 public class Ingredient {
+    @Id
     private final String id;
     private final String name;
     private final Type type;
+
     public enum Type {
         WRAP, PROTEIN, VEGGIES, CHEESE, SAUCE
     }
--- src/main/java/com/project/tacocloud/model/TacoOrder.java
+++ src/main/java/com/project/tacocloud/model/TacoOrder.java
@@ -4,18 +4,23 @@ import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
-@NoArgsConstructor
-public class TacoOrder {
+@Table
+public class TacoOrder implements Serializable {
 
+    @Serial
     private static final long serialVersionUID = 1L;
+    @Id
     private Long id;
     private Date placedAt;
     @NotBlank(message = "Delivery name is required")
@@ -30,7 +35,7 @@ public class TacoOrder {
     private String deliveryZip;
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
-    @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+    @Pattern(regexp = "^(0[1-9]|1[0-2])(/)([2-9][0-9])$",
             message = "Must be formatted MM/YY")
     private String ccExpiration;
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
